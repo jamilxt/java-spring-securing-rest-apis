57803513efbde963eef5c630afdb72ed466033b6
Jamilur Rahman <jamilur.rahman@bs-23.net>
2021-04-19 02:54:48 +0600

    Completed module

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -2,13 +2,31 @@ package io.jzheaux.springsecurity.resolutions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class ResolutionsApplication {
+import static org.springframework.http.HttpMethod.*;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ResolutionsApplication.class, args);
-	}
+@SpringBootApplication
+public class ResolutionsApplication extends WebSecurityConfigurerAdapter {
 
+  public static void main(String[] args) {
+    SpringApplication.run(ResolutionsApplication.class, args);
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService(UserRepository users) {
+    return new UserRepositoryUserDetailsService(users);
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests(authz -> authz
+        .mvcMatchers(GET, "resolutions", "resolutions/**").hasAnyAuthority("resolution:read")
+        .anyRequest().hasAnyAuthority("resolution:write"))
+        .httpBasic(basic -> {
+        });
+  }
 }
